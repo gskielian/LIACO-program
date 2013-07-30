4cb01a850d97bb04f3d7a4d7fb64d3b1cc992042
chessmanlo <chessmanlo@gmail.com>
2013-07-29 19:50:29 -0700

    LIACO.ino

--- LIACO.ino
+++ LIACO.ino
@@ -1,15 +1,3 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 12;
-
-// the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);     
@@ -27,7 +15,6 @@ void setup() {
   pinMode(12, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
   
   for ( int i = 0; i <= 12 ; i += 1 )
